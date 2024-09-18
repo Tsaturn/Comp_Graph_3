fb4054c68ff2a6f430a3ace373a2e5d8e454db90
Maria <maria_tsaturyan@mail.ru>
2024-09-18 15:21:27 +0300

    Add forms

--- Lab3/Form1.cs
+++ Lab3/Form1.cs
@@ -12,9 +12,34 @@ namespace Lab3
 {
     public partial class Form1 : Form
     {
+        protected Form task1, task2, task3;
+
         public Form1()
-        {
+        { 
             InitializeComponent();
+            task1 = new Task1();
+            task1.Hide();
+
+            task2 = new Task2();
+            task2.Hide();
+
+            task3 = new Task3();
+            task3.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            task1.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            task2.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            task3.ShowDialog();
         }
     }
 }
---
+++ Lab3/Task1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Task1 : Form
+    {
+        public Task1()
+        {
+            InitializeComponent();
+        }
+
+        private void Task1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
