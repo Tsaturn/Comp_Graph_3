f1110165f3ce61a7e15900f7522b88372f0518db
anastarrr <92821335+anastarrr@users.noreply.github.com>
2024-09-27 15:59:41 +0300

    Task2

--- Lab3/Task2.Designer.cs
+++ Lab3/Task2.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Task2
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,43 +13,83 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBoxBresenham = new System.Windows.Forms.CheckBox();
+            this.checkBoxWu = new System.Windows.Forms.CheckBox();
+            this.clearButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(225, 1);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(349, 329);
+            this.pictureBox1.Size = new System.Drawing.Size(657, 464);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // checkBoxBresenham
+            // 
+            this.checkBoxBresenham.AutoSize = true;
+            this.checkBoxBresenham.Location = new System.Drawing.Point(14, 77);
+            this.checkBoxBresenham.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.checkBoxBresenham.Name = "checkBoxBresenham";
+            this.checkBoxBresenham.Size = new System.Drawing.Size(205, 24);
+            this.checkBoxBresenham.TabIndex = 1;
+            this.checkBoxBresenham.Text = "Алгоритм Брезенхема";
+            this.checkBoxBresenham.UseVisualStyleBackColor = true;
+            this.checkBoxBresenham.CheckedChanged += new System.EventHandler(this.checkBoxBresenham_CheckedChanged);
+            // 
+            // checkBoxWu
+            // 
+            this.checkBoxWu.AutoSize = true;
+            this.checkBoxWu.Location = new System.Drawing.Point(14, 119);
+            this.checkBoxWu.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.checkBoxWu.Name = "checkBoxWu";
+            this.checkBoxWu.Size = new System.Drawing.Size(132, 24);
+            this.checkBoxWu.TabIndex = 2;
+            this.checkBoxWu.Text = "Алгоритм Ву";
+            this.checkBoxWu.UseVisualStyleBackColor = true;
+            this.checkBoxWu.CheckedChanged += new System.EventHandler(this.checkBoxWu_CheckedChanged);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(14, 13);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(112, 38);
+            this.clearButton.TabIndex = 3;
+            this.clearButton.Text = "Очистить";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
             // 
             // Task2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.ClientSize = new System.Drawing.Size(882, 461);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.checkBoxWu);
+            this.Controls.Add(this.checkBoxBresenham);
             this.Controls.Add(this.pictureBox1);
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Task2";
-            this.Text = "Task2";
+            this.Text = "Отрисовка отрезков";
             this.Load += new System.EventHandler(this.Task2_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
-        #endregion
 
+        private System.Windows.Forms.CheckBox checkBoxBresenham;
+        private System.Windows.Forms.CheckBox checkBoxWu;
+        private System.Windows.Forms.Button clearButton;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Lab3/Task2.cs
+++ Lab3/Task2.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Lab3
@@ -19,7 +13,121 @@ namespace Lab3
 
         private void Task2_Load(object sender, EventArgs e)
         {
+        }
 
+        private void BresenhamLine(int x0, int y0, int x1, int y1)
+        {
+            int dx = Math.Abs(x1 - x0);
+            int dy = Math.Abs(y1 - y0);
+            int sx = x0 < x1 ? 1 : -1;
+            int sy = y0 < y1 ? 1 : -1;
+            int err = dx - dy;
+
+            while (true)
+            {
+                DrawPoint(x0, y0); 
+
+                if (x0 == x1 && y0 == y1) break;
+
+                int e2 = 2 * err;
+                if (e2 > -dy)
+                {
+                    err -= dy;
+                    x0 += sx;
+                }
+
+                if (e2 < dx)
+                {
+                    err += dx;
+                    y0 += sy;
+                }
+            }
+        }
+
+        private void WuLine(int x0, int y0, int x1, int y1)
+        {
+            float dx = x1 - x0;
+            float dy = y1 - y0;
+            float gradient = dy / dx;
+
+            if (x0 > x1)
+            {
+                int tempX = x0;
+                int tempY = y0;
+                x0 = x1;
+                y0 = y1;
+                x1 = tempX;
+                y1 = tempY;
+            }
+
+            float y = y0 + gradient;
+
+            for (var x = x0 + 1; x <= x1 - 1; x++)
+            {
+                DrawPoint(x, (int)y, 1 - (y - (int)y)); 
+                DrawPoint(x, (int)y + 1, y - (int)y);   
+                y += gradient;
+            }
+        }
+
+
+        private void DrawPoint(int x, int y, float intensity = 1)
+        {
+            if (intensity == 1)
+            {
+                using (Graphics g = pictureBox1.CreateGraphics())
+                {
+                    g.FillRectangle(Brushes.Black, x, y, 2, 2);
+                }
+            }
+            else
+            {
+                using (Graphics g = pictureBox1.CreateGraphics())
+                {
+                    Color color = Color.FromArgb((int)(intensity * 255), Color.Black);
+                    Brush brush = new SolidBrush(color);
+                    g.FillRectangle(brush, x, y, 2, 2);
+                }
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int x0 = 150;  
+            int y0 = 100;
+            int x1 = e.X;
+            int y1 = e.Y;
+
+            if (checkBoxBresenham.Checked)
+            {
+                BresenhamLine(x0, y0, x1, y1);
+            }
+            else if (checkBoxWu.Checked)
+            {
+                WuLine(x0, y0, x1, y1);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+            pictureBox1.Invalidate(); 
+        }
+
+        private void checkBoxBresenham_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxBresenham.Checked)
+            {
+                checkBoxWu.Checked = false; 
+            }
+        }
+
+        private void checkBoxWu_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxWu.Checked)
+            {
+                checkBoxBresenham.Checked = false; 
+            }
         }
     }
 }
