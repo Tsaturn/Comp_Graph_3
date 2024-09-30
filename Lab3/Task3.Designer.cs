12a7871070c1f7ee86a155592fc1b3d006eb8bf2
Maria <maria_tsaturyan@mail.ru>
2024-09-30 20:25:02 +0300

    MEOW

--- Lab3/Task3.Designer.cs
+++ Lab3/Task3.Designer.cs
@@ -29,26 +29,50 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(39, 42);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(26, 27);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(810, 558);
+            this.pictureBox1.Size = new System.Drawing.Size(540, 357);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(770, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Выбрать цвет точки";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(770, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Task3
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1200, 703);
+            this.ClientSize = new System.Drawing.Size(945, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Task3";
             this.Text = "Task3";
             this.Load += new System.EventHandler(this.Task3_Load);
@@ -60,5 +84,8 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Lab3/Task3.cs
+++ Lab3/Task3.cs
@@ -12,9 +12,17 @@ namespace Lab3
 {
     public partial class Task3 : Form
     {
+        private Color PointColor = Color.Black;
+        private Graphics g;
+        private Queue<(Point, Color)> points = new Queue<(Point, Color)>();
+        private int count = 0;
+
         public Task3()
         {
             InitializeComponent();
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image);
+            g.Clear(Color.White);
         }
 
         private void Task3_Load(object sender, EventArgs e)
@@ -22,9 +30,45 @@ namespace Lab3
 
         }
 
-        private void pictureBox1_Click(object sender, EventArgs e)
-        {
 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                if (colorDialog1.ShowDialog() == DialogResult.Cancel)
+                    return;
+                PointColor = colorDialog1.Color;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point point = e.Location;
+            count++;
+
+            float x = e.Location.X;
+            float y = e.Location.Y;
+            g.FillRectangle(new SolidBrush(PointColor), e.Location.X, e.Location.Y, 2, 2);
+            pictureBox1.Refresh();
+
+            points.Enqueue((point, PointColor));
+            if (count % 3 == 0)
+                DrawTriangle();
+        }
+
+        private void DrawTriangle()
+        {
+            (Point, Color) p1 = points.Dequeue();
+            (Point, Color) p2 = points.Dequeue();
+            (Point, Color) p3 = points.Dequeue();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            g.Clear(Color.White);
+            pictureBox1.Refresh();
+            count = 0;
+            points.Clear();
         }
     }
 }
