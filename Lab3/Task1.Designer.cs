4e0bdec39648100b16a9d1866deeed0638cde8ba
elizave-taa <114254752+elizave-taa@users.noreply.github.com>
2024-10-02 13:54:19 +0300

    Revert "Merge branch 'Task1'"
    This reverts commit 0337bbd47a49de00a84695809e1b088ad37b2de5, reversing
    changes made to 52058dd5399a92d89a4c43bfa177d9e410ab2a8d.

--- Lab3/Task1.Designer.cs
+++ Lab3/Task1.Designer.cs
@@ -28,181 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.trackBar1 = new System.Windows.Forms.TrackBar();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button7 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(806, 825);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
-            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
-            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(849, 29);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(167, 48);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Карандаш";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.pen_button);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(1033, 31);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(161, 46);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Заливка";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.fill_button);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(849, 232);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(317, 47);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Выбрать цвет";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.buttonSelectColor);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(896, 742);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(286, 52);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "Очистить холст";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.clear_button);
             // 
             // pictureBox2
             // 
-            this.pictureBox2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.pictureBox2.Location = new System.Drawing.Point(849, 298);
+            this.pictureBox2.Location = new System.Drawing.Point(31, 38);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(333, 232);
-            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.Size = new System.Drawing.Size(549, 611);
+            this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
+            this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseDown_1);
+            this.pictureBox2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseMove_1);
+            this.pictureBox2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox2_MouseUp_1);
             // 
-            // button5
+            // button1
             // 
-            this.button5.Enabled = false;
-            this.button5.Location = new System.Drawing.Point(849, 592);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(275, 57);
-            this.button5.TabIndex = 6;
-            this.button5.Text = "Заливка изображением";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.fillImage_button);
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(849, 536);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(275, 50);
-            this.button6.TabIndex = 7;
-            this.button6.Text = "Загрузить";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Click += new System.EventHandler(this.UploadImageButton);
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            // 
-            // trackBar1
-            // 
-            this.trackBar1.Location = new System.Drawing.Point(849, 136);
-            this.trackBar1.Maximum = 5;
-            this.trackBar1.Minimum = 1;
-            this.trackBar1.Name = "trackBar1";
-            this.trackBar1.Size = new System.Drawing.Size(365, 90);
-            this.trackBar1.TabIndex = 8;
-            this.trackBar1.Value = 1;
-            this.trackBar1.Scroll += new System.EventHandler(this.thicknessChange);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(857, 98);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(168, 25);
-            this.label1.TabIndex = 9;
-            this.label1.Text = "Толщина линии";
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(849, 675);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(275, 48);
-            this.button7.TabIndex = 10;
-            this.button7.Text = "Выделить границу";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Click += new System.EventHandler(this.hightlight_button);
+            this.button1.Location = new System.Drawing.Point(655, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 77);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Выбрать цвет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Task1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1287, 883);
-            this.Controls.Add(this.button7);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.trackBar1);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(1200, 703);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Controls.Add(this.pictureBox2);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Task1";
             this.Text = "Task1";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.ColorDialog colorDialog1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+
         private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.TrackBar trackBar1;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button1;
     }
 }
